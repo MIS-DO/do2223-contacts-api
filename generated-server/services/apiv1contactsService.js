a7c8dce6abae05a5922c001bddd427a0596d3353
josemgarcia <josemgarcia@us.es>
2023-01-26 22:58:34 +0100

    C01 version

---
+++ generated-server/services/apiv1contactsService.js
@@ -0,0 +1,12 @@
+module.exports.getContact = function getContact(req, res) {
+    res.send({
+        message: 'This is the mockup controller for getContact'
+    });
+}
+
+module.exports.addContact = function addContact(req, res) {
+    res.send({
+        message: 'This is the mockup controller for addContact'
+    });
+}
+
---
+++ generated-server/services/apiv1contactsnameService.js
@@ -0,0 +1,18 @@
+module.exports.findByname = function findByname(req, res) {
+    res.send({
+        message: 'This is the mockup controller for findByname'
+    });
+}
+
+module.exports.updateContact = function updateContact(req, res) {
+    res.send({
+        message: 'This is the mockup controller for updateContact'
+    });
+}
+
+module.exports.deleteContact = function deleteContact(req, res) {
+    res.send({
+        message: 'This is the mockup controller for deleteContact'
+    });
+}
+
